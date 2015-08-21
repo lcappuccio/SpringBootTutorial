158aff66c78e904c14ea3a2df87222bb5037fa9f
Leonardo Cappuccio <leo@systemexception.org>
2015-08-22 00:46:10 +0200

    green: get_a_message

---
+++ src/test/java/org/systemexception/springboottutorial/test/HelloControllerTest.java
@@ -0,0 +1,41 @@
+package org.systemexception.springboottutorial.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.systemexception.springboottutorial.controller.HelloController;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author leo
+ * @date 22/08/15 00:35
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class HelloControllerTest {
+
+	private MockMvc sut;
+
+	@Before
+	public void setUp() {
+		sut = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
+	@Test
+	public void get_a_message() throws Exception {
+		sut.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("SpringBoot working")));
+	}
+}
