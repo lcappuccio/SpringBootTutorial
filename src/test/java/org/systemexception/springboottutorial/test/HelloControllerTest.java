c0e9629854edeeb858ba41500161186fbe81d680
Leonardo Cappuccio <leo@systemexception.org>
2015-08-25 21:20:50 +0200

    green: get_a_message

--- src/test/java/org/systemexception/springboottutorial/test/HelloControllerTest.java
+++ src/test/java/org/systemexception/springboottutorial/test/HelloControllerTest.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,19 +24,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @date 22/08/15 00:35
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
-@ContextConfiguration(classes = { HelloControllerConfig.class }, loader = AnnotationConfigContextLoader.class)
+@SpringApplicationConfiguration(classes = {MockServletContext.class, HelloController.class, HelloControllerConfig
+		.class})
 @WebAppConfiguration
 public class HelloControllerTest {
 
 	// This test mocks the servlet request and response of the endpoint
 	@Autowired
 	private HelloControllerConfig helloControllerConfig;
+	@Autowired
+	private HelloController helloController;
 	private MockMvc sut;
 
 	@Before
 	public void setUp() {
-		sut = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+		sut = MockMvcBuilders.standaloneSetup(helloController).build();
 	}
 
 	@Test
