00d4450a39fedcc32935964fc8899191c6dc4a89
Leonardo Cappuccio <leo@systemexception.org>
2015-08-25 20:18:01 +0200

    remove unused setter

--- src/main/java/org/systemexception/springboottutorial/Config/HelloControllerConfig.java
+++ src/main/java/org/systemexception/springboottutorial/Config/HelloControllerConfig.java
@@ -18,8 +18,4 @@ public class HelloControllerConfig {
 	public String getHelloMessage() {
 		return helloMessage;
 	}
-
-	public void setHelloMessage(String helloMessage) {
-		this.helloMessage = helloMessage;
-	}
 }
